20e30f34ef758e806552a73696c0b587ff6a1487
Raul <ravega@gmail.com>
2017-01-03 10:06:15 +0000

    Updating projects with basic insert

---
+++ MBussines/ejbModule/com/ravega/bean/session/MainSession.java
@@ -0,0 +1,63 @@
+package com.ravega.bean.session;
+
+import java.util.List;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateful;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import com.ravega.bean.entity.Messages;
+
+/**
+ * Session Bean implementation class MainSession
+ */
+@Stateful
+@LocalBean
+public class MainSession implements MainSessionRemote, MainSessionLocal {
+
+	@PersistenceContext(unitName="MBussines")
+	private EntityManager entityManager;
+	
+    /**
+     * Default constructor. 
+     */
+    public MainSession() {
+        // TODO Auto-generated constructor stub
+    }
+    
+    public List<Messages> getMessages() {
+    	TypedQuery<Messages> query =
+    			entityManager.createQuery("SELECT c FROM Messages c", Messages.class);
+    	//Query query = entityManager.createQuery("SELECT message FROM Messages m");
+        List<Messages> resultList = (List<Messages>) query.getResultList();	        
+		return resultList;
+    }
+    
+    public String getMessage() {
+    	return "";
+    }
+    
+    public void setMessage(Messages m_) {    	
+    	//message = new Messages();
+    	//message.setMessage(m_);
+    	if (entityManager == null) {
+    		System.out.println("Null EM");    	
+    	} else {
+    		System.out.println("Persisting Message"); 
+    		entityManager.persist(m_);
+    	}
+    }
+    
+    public void deleteMessage(Messages m_) {
+    	if (entityManager == null) {
+    		System.out.println("Null EM");    	
+    	} else {
+    		System.out.println("Delete Message: " + m_.getId() + " : " + m_.getMessage());
+    		entityManager.remove(entityManager.find(Messages.class,  m_.getId()));
+    		System.out.println("Message Deleted");
+    	}     		
+    }
+
+}
---
+++ MView/src/main/java/com/ravega/view/jsf/MView.java
@@ -0,0 +1,101 @@
+package com.ravega.view.jsf;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Properties;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.component.html.HtmlInputText;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import com.ravega.bean.entity.Messages;
+import com.ravega.bean.session.MainSessionRemote;
+
+@ManagedBean(name = "mView", eager = true)
+@RequestScoped
+public class MView {
+
+	private HtmlInputText message;
+	ArrayList<Messages> messages = new ArrayList<Messages>();	
+
+	public MView() {
+		System.out.println("MView started!");
+		readMessages();
+	}
+	
+	public ArrayList<Messages> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(ArrayList<Messages> messages) {
+		this.messages = messages;
+	}
+
+	public void setMessage(HtmlInputText message) {
+		this.message = message;
+	}
+	
+	public HtmlInputText getMessage() {
+	      return message;
+	   }
+	
+	public String moveToInsertMessage() {
+	      return "insert";
+	   }
+
+	public String moveToInsertAndHome() {
+		insertNewMessage();
+		return "home";
+	}
+
+	private void readMessages() {
+		MainSessionRemote msr = getSessionRemote();			
+		messages = (ArrayList<Messages>)msr.getMessages();
+		Iterator<Messages> it = messages.iterator();			
+		while (it.hasNext()) {
+			System.out.println(((Messages)it.next()).getMessage());
+		}
+	}
+
+	private void insertNewMessage() {
+		System.out.println(message.getValue());
+		
+		MainSessionRemote msr = getSessionRemote();
+		Messages message_bean = new Messages();
+		message_bean.setMessage((String)message.getValue());
+		msr.setMessage(message_bean);
+		readMessages();
+	}
+	
+	public String deleteMessage(Messages m_) {
+		System.out.println("Delete: " + m_.getMessage());
+		MainSessionRemote msr = getSessionRemote();		
+		msr.deleteMessage(m_);
+		readMessages();
+		return "home";
+	}
+	
+	private MainSessionRemote getSessionRemote() {
+        Properties props = new Properties();
+		
+		props.put("java.naming.factory.initial", "org.jnp.interfaces.NamingContextFactory");
+		props.put("java.naming.factory.url.pkgs", "org.jboss.naming:org.jnp.interfaces");
+		props.put(Context.PROVIDER_URL, "localhost");
+		
+		try {
+			InitialContext ctx = new InitialContext();
+			
+			MainSessionRemote msr = (MainSessionRemote)ctx.lookup("java:global/MyBussines/MBussines/MainSession!com.ravega.bean.session.MainSessionRemote");
+			return msr;
+			
+		} catch (NamingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+}
